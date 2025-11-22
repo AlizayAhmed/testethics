29677a17cf8de652114f6efca9d6b6a71bcb9469
Alizay Ahmed <159795390+AlizayAhmed@users.noreply.github.com>
2025-11-23 01:19:07 +0500

    Create test_ip.py

---
+++ test_ip.py
@@ -0,0 +1,16 @@
+"""
+Copyright © 2025 StellarSoft Inc. – All Rights Reserved.
+This file is for testing IP & Copyright detection.
+Unauthorized reproduction is prohibited.
+"""
+
+def proprietary_algorithm(data):
+    # Dummy algorithm
+    return sum(ord(c) for c in data) % 256
+
+if __name__ == "__main__":
+    sample = "Test data for IP & Copyright"
+    print(proprietary_algorithm(sample))
+
+api_key = "sk-1234567890abcdef"
+
